97c5d64deffe1f620ee95d5d7ecf058527d83a86
Omprakash Sridharan <omprakashsridharan@outlook.com>
2021-10-08 00:04:17 +0200

    Adds excel sheet column title optimised version

--- src/excel_sheet_column_title.rs
+++ src/excel_sheet_column_title.rs
@@ -1,23 +1,16 @@
 pub fn run() {
     println!("========");
     println!("Excel sheet column title");
-    println!("{:?}", convert_to_title(703));
+    println!("{:?}", convert_to_title(2147483647));
     println!("========");
 }
 
 pub fn convert_to_title(column_number: i32) -> String {
     let mut result = String::new();
-    let mut quotient = (column_number - 1) / 26;
-    let remainder = (column_number - 1) % 26;
-    loop {
-        if quotient > 0 {
-            result.push(((quotient - 1) % 26 + 65) as u8 as char);
-            quotient = (quotient - 1) / 26;
-        } else {
-            result = result.chars().rev().collect::<String>();
-            result.push((remainder + 65) as u8 as char);
-            break;
-        }
+    let mut num = column_number;
+    while num > 0 {
+        result.push((65 + ((num - 1) % 26)) as u8 as char);
+        num = (num - 1) / 26;
     }
-    return result;
+    return result.chars().rev().collect::<String>();
 }
