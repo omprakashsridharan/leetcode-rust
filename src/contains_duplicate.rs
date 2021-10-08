03045c0b1a773caa46b24b426e5cd3846f872dc4
Omprakash Sridharan <omprakashsridharan@outlook.com>
2021-10-08 17:45:51 +0200

    Adds contains_duplicate

---
+++ src/contains_duplicate.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+pub fn run() {
+    println!("========");
+    println!("Excel sheet column title");
+    println!("{:?}", contains_duplicate(vec![1, 2, 3, 2]));
+    println!("========");
+}
+
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut map: HashMap<i32, bool> = HashMap::new();
+    for num in nums {
+        if let Some(_) = map.get(&num) {
+            return true;
+        } else {
+            map.insert(num, true);
+        }
+    }
+    return false;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+pub mod contains_duplicate;
 pub mod excel_sheet_column_title;
 pub mod two_sum;
 pub mod two_sum_sorted;
@@ -7,4 +8,5 @@ fn main() {
     two_sum::run();
     two_sum_sorted::run();
     excel_sheet_column_title::run();
+    contains_duplicate::run();
 }
